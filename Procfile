bddba5b96a33937fdf3d1ebc20fc1cbd0c4d5f1a
mod26-k <kellirinaw@gmail.com>
2023-05-11 07:45:51 -1000

    deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn eros.wsgi
--- eros/settings.py
+++ eros/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+environ.Env()
+environ.Env.read_env()
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a(80w@4lkxz=r&*5s7ym302qawd%oedq_ogi^d9hodo5)n2ycp'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.environ['MODE'] == 'dev' else False
 
 ALLOWED_HOSTS = []
 
@@ -77,7 +81,11 @@ WSGI_APPLICATION = 'eros.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'eros',
+        'NAME': 'kellirinaw/eros',
+        'USER': 'kellirinaw',
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': 'db.bit.io',
+        'PORT': '5432',
     }
 }
 
